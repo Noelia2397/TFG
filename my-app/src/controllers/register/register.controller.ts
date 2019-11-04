98546e9501b852265c417306c60ac1013bb3c868
Noelia <noelia2397@gmail.com>
2019-11-04 18:02:52 +0100

    Traer datos base de datos y config bbdd

--- my-app/src/controllers/register/register.controller.ts
+++ my-app/src/controllers/register/register.controller.ts
@@ -2,20 +2,18 @@ import {RegisterRequest, AsignResponseDto} from "./register-dtos";
 import {Subject, Observable} from 'rxjs';
 import firebase from 'firebase';
 import { RegisterDto } from "../../components/register/register-dto";
+import {firebaseConfig} from "../../config/config-bbdd";
+//import 'firebase/database';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBKosyJzR1yXLe7I39vaXJAtHJplgsOrQc",
-    authDomain: "app-ble-bbdd.firebaseapp.com",
-    databaseURL: "https://app-ble-bbdd.firebaseio.com",
-    projectId: "app-ble-bbdd",
-    storageBucket: "app-ble-bbdd.appspot.com",
-    messagingSenderId: "439902971846",
-    appId: "1:439902971846:web:59b405049c8da04c4534c7"
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
 
 export class RegisterController{
+
+    constructor(){
+        console.log("Estoy en el constructor");
+        firebase.initializeApp(firebaseConfig);
+    }
     private _numBeaconAsign: Subject<RegisterDto> = new Subject();
 
     public onViewDtoChangeReceived(): Observable<RegisterDto>{
@@ -45,11 +43,16 @@ export class RegisterController{
     public async registerUser (registerRequest: RegisterRequest){
         console.log("Estoy en register controller");
     
-        const auxd = firebase.database().ref().child('users').child('name');
+        /*const auxd = firebase.database().ref().child('users').child('name');
         console.log(auxd);
         console.log("este es el bueno ")
         auxd.on('value', function(e) {
             console.log(e.val())
+        });*/
+        const notes=registerRequest;
+        const auxd= firebase.database().ref('users/').child(registerRequest.user_hist).set({
+            userName: registerRequest.user_name,
+            beacon: registerRequest.beacon,
         });
         console.log("Impreso");
         const response: AsignResponseDto = {
