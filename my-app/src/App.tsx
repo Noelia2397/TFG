826e658c22655338b4eabc1b4ddc7c1bb2b28c56
Noelia <noelia2397@gmail.com>
2019-11-15 13:23:52 +0100

    Bootstrap

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './assets/css/main.css';
-import './assets/css/reset.css'
+import './assets/css/reset.css';
+import './assets/css/bootstrap.scss';
 import { RouterViewsPath } from './components/router/router';
 
 const App: React.FC = () => {
